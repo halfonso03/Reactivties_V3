40ee5954797a30a156b81d2c0c9c5a53ba1e50ce
admin <hector.alfonso@yahoo.com>
2025-08-10 16:18:17 -0400

    End of Section 5

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import { Box, Container, CssBaseline } from "@mui/material";
+import axios from "axios";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+
+function App() {
+	const [activities, setActivities] = useState<Activity[]>([]);
+	const [selectedActivity, setSelectedActivity] = useState<
+		Activity | undefined
+	>(undefined);
+
+	const [editMode, setEditMode] = useState(false);
+
+	useEffect(() => {
+		axios
+			.get<Activity[]>("https://localhost:5001/api/activities")
+			.then((response) => setActivities(response.data));
+
+		return () => {};
+	}, []);
+
+	const handleSelectActivity = (id: string) => {
+		setSelectedActivity(activities.find((x) => x.id === id));
+	};
+
+	const handleCancelSelectActivity = () => {
+		setSelectedActivity(undefined);
+	};
+
+	const handleOpenForm = (id?: string) => {
+		if (id) handleSelectActivity(id);
+		else handleCancelSelectActivity();
+		setEditMode(true);
+	};
+
+	const handleFormClose = () => {
+		setEditMode(false);
+	};
+
+	const handleSubmitForm = (activity: Activity) => {
+		if (activity.id) {
+			setActivities(
+				activities.map((x) => {
+					if (x.id === activity.id) {
+						x = activity;
+						return x;
+					} else {
+						return x;
+					}
+				})
+			);
+		} else {
+			const newActivity = {
+				...activity,
+				id: activities.length.toString(),
+			};
+			setSelectedActivity(newActivity);
+			setActivities([...activities, newActivity]);
+		}
+
+		setEditMode(false);
+	};
+
+	const handleDelete = (id: string) => {
+		setActivities(activities.filter((x) => x.id !== id));
+	};
+
+	return (
+		<Box sx={{ bgcolor: "#eeeeee" }}>
+			<CssBaseline />
+			<NavBar openForm={handleOpenForm} />
+			<Container
+				maxWidth='xl'
+				sx={{ marginTop: 3 }}>
+				<ActivityDashboard
+					activities={activities}
+					selectActivity={handleSelectActivity}
+					cancelSelectActivity={handleCancelSelectActivity}
+					selectedActivity={selectedActivity}
+					openForm={handleOpenForm}
+					editMode={editMode}
+					closeForm={handleFormClose}
+					submitForm={handleSubmitForm}
+					deleteActivity={handleDelete}></ActivityDashboard>
+			</Container>
+		</Box>
+	);
+}
+
+export default App;
---
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,58 @@
+import { Grid2 } from "@mui/material";
+import ActivityList from "./ActivityList";
+import ActivityDetail from "../details/ActivityDetails";
+import ActivityForm from "./form/ActivityForm";
+
+type Props = {
+	activities: Activity[];
+	selectedActivity?: Activity;
+	selectActivity: (id: string) => void;
+	cancelSelectActivity: () => void;
+	openForm: (id: string) => void;
+	closeForm: () => void;
+	editMode: boolean;
+	submitForm: (activity: Activity) => void;
+	deleteActivity: (id: string) => void;
+};
+
+export default function ActivityDashboard({
+	activities,
+	selectedActivity,
+	selectActivity,
+	cancelSelectActivity,
+	openForm,
+	editMode,
+	closeForm,
+	submitForm,
+	deleteActivity,
+}: Props) {
+	return (
+		<Grid2
+			container
+			spacing={3}>
+			<Grid2 size={7}>
+				<ActivityList
+					activities={activities}
+					selectActivity={selectActivity}
+					deleteActivity={deleteActivity}
+				/>
+			</Grid2>
+			<Grid2 size={5}>
+				{selectedActivity && !editMode && (
+					<ActivityDetail
+						activity={selectedActivity}
+						cancelSelectActivity={cancelSelectActivity}
+						openForm={openForm}
+					/>
+				)}
+				{editMode && (
+					<ActivityForm
+						submitForm={submitForm}
+						activity={selectedActivity}
+						closeForm={closeForm}
+					/>
+				)}
+			</Grid2>
+		</Grid2>
+	);
+}
