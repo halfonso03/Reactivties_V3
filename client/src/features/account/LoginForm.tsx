7e5b366526e4619fd7085749e8560ffdf256fb6b
admin <hector.alfonso@yahoo.com>
2025-08-16 15:31:20 -0400

    end of section 13

---
+++ client/src/features/account/LoginForm.tsx
@@ -0,0 +1,84 @@
+import { useForm } from "react-hook-form";
+
+import { loginSchema, LoginSchema } from "../../lib/schemas/loginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import { LockOpen } from "@mui/icons-material";
+import TextInput from "../../app/shared/components/TextInput";
+import { Link, useLocation, useNavigate } from "react-router";
+import useAccount from "../../lib/hooks/useAccount";
+
+export default function LoginForm() {
+	const { loginUser } = useAccount();
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	const {
+		control,
+		handleSubmit,
+		formState: { isValid, isSubmitting },
+	} = useForm<LoginSchema>({
+		mode: "onTouched",
+		resolver: zodResolver(loginSchema),
+	});
+
+	const onSubmit = async (data: LoginSchema) => {
+		await loginUser.mutateAsync(data, {
+			onSuccess: () => {
+				console.log(location.state?.from);
+				navigate("/activities");
+			},
+		});
+	};
+
+	return (
+		<Paper
+			component={"form"}
+			onSubmit={handleSubmit(onSubmit)}
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				p: 3,
+				gap: 3,
+				maxWidth: "md",
+				mx: "auto",
+				borderRadius: 3,
+			}}>
+			<Box
+				display='flex'
+				alignItems='center'
+				justifyContent='center'
+				gap={3}
+				color='secondary.main'>
+				<LockOpen fontSize='large'></LockOpen>
+				<Typography variant='h4'>Sign In</Typography>
+			</Box>
+			<TextInput
+				label='Email'
+				control={control}
+				name='email'></TextInput>
+			<TextInput
+				label='Password'
+				control={control}
+				type='password'
+				name='password'></TextInput>
+			<Button
+				type='submit'
+				disabled={!isValid || isSubmitting}
+				variant='contained'
+				size='large'>
+				Login
+			</Button>
+			<Typography sx={{ textAlign: "center" }}>
+				Don't have an account?
+				<Typography
+					sx={{ ml: 2 }}
+					component={Link}
+					to='/register'
+					color='primary'>
+					Sign up
+				</Typography>
+			</Typography>
+		</Paper>
+	);
+}
--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -3,11 +3,12 @@ import ActivityCard from "./ActivityCard";
 import { useActivities } from "../../../lib/hooks/useActivities";
 
 export default function ActivityList() {
-	const { activities, isPending } = useActivities();
+	const { activities, isLoading } = useActivities();
 
-	if (!activities || isPending) {
+	if (isLoading) {
 		return <Typography>Loading...</Typography>;
 	}
+	if (!activities) return <Typography>No activitie found</Typography>;
 
 	return (
 		<Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
